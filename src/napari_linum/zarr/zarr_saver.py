761570eff84cbc86914a8ceb4105d9443e0b1cc0
comtois-etienne <comtois.etienne@outlook.com>
2024-04-16 13:09:11 -0400

    zarr save with compression

--- src/napari_linum/zarr/zarr_saver.py
+++ src/napari_linum/zarr/zarr_saver.py
@@ -1,14 +1,44 @@
 from typing import TYPE_CHECKING
 
-from magicgui.widgets import CheckBox, create_widget, PushButton, EmptyWidget, Label, FileEdit, FloatSpinBox
+from magicgui.widgets import CheckBox, create_widget, PushButton, EmptyWidget, Label, FileEdit, FloatSpinBox, Select, ComboBox
 import zarr
+import numcodecs
 import numpy as np
+import os
 from os import path
 
 from ..widget import LinumWidget
 
 if TYPE_CHECKING:
     import napari
+
+
+def is_dir_empty(directory, ignore_hidden=True):
+    if not path.exists(directory):
+        return True
+    files = os.listdir(directory)
+    if ignore_hidden:
+        files = [file for file in files if not file.startswith('.')]
+    return not files
+
+
+def compress_zarr(zarr_arr, method):
+    # Create a compressor object based on the specified method
+    if method == "none":
+        return zarr_arr
+    elif method == "gzip":
+        compressor = numcodecs.GZip(level=1)
+    elif method == "zstd":
+        compressor = numcodecs.Zstd(level=1)
+    elif method == "lz4":
+        compressor = numcodecs.LZ4(level=1)
+    elif method.startswith("blosc:"):
+        blosc_method = method.split(":")[1]
+        compressor = numcodecs.Blosc(cname=blosc_method, clevel=5, shuffle=numcodecs.Blosc.SHUFFLE)
+    else:
+        raise ValueError(f"Unsupported compression method: {method}")
+
+    return zarr.array(zarr_arr, compressor=compressor)
 
 
 class ZarrSaver(LinumWidget):
@@ -20,23 +50,50 @@ class ZarrSaver(LinumWidget):
         )
         self._zarr_path = FileEdit(label="Save Directory", mode='d')
 
-        self._save_button = PushButton(text="Save Zarr")
+        self._format = ComboBox(
+            label="Format",
+            choices=["zarr", "ome-zarr"],
+            value="zarr",
+        )
+
+        self._compression = ComboBox(
+            label="Compression",
+            choices=["none", "gzip", "zstd", "lz4"] + [f"blosc:{x}" for x in numcodecs.blosc.list_compressors()],
+            value="gzip",
+        )
+
+        self._overwrite = CheckBox(text="Overwrite", value=False)
+
+        self._save_button = PushButton(text="Save Layer")
         self._save_button.changed.connect(self._save_zarr)
 
         self.extend(
             [
                 self._source_layer,
                 self._zarr_path,
+                self._format,
+                self._compression,
+                self._overwrite,
                 self._save_button,
             ]
         )
 
     def _save_zarr(self):
-        annotations = self._source_layer.value.data
-        layer_name = self._source_layer.value.name
+        data = self._source_layer.value.data
+
         zarr_path = str(self._zarr_path.value)
-        z = zarr.zeros(shape=annotations.shape, dtype=annotations.dtype)
-        z[:] = annotations
-        save_path = path.join(zarr_path, f'{layer_name}.zarr')
-        zarr.save(save_path, z)
+        if not zarr_path.endswith('.zarr'):
+            layer_name = self._source_layer.value.name
+            zarr_path = path.join(zarr_path, f'{layer_name}.zarr')
+
+        if not self._overwrite.value and not is_dir_empty(zarr_path):
+            self._update_message('File already exists. Enable overwrite to save.')
+            return
+
+        self._update_message('Saving Zarr file...')
+        z = zarr.zeros(shape=data.shape, dtype=data.dtype)
+        z[:] = data
+        z = compress_zarr(z, self._compression.value)
+        zarr.save(zarr_path, z)
+        self._update_message('Zarr file saved')
 
