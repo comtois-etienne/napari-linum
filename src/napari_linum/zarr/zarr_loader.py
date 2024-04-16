b3c6669f4b379225aa0c343ba2f57b5b875a837a
comtois-etienne <comtois.etienne@outlook.com>
2024-04-16 15:30:23 -0400

    omezarr loader

--- src/napari_linum/zarr/zarr_loader.py
+++ src/napari_linum/zarr/zarr_loader.py
@@ -15,19 +15,16 @@ def get_name(fpath):
     return path.basename(fpath).split(".")[0]
 
 
+def get_extension(fpath):
+    return str(path.basename(fpath).split(".")[-1]).lower()
+
+
 class ZarrLoader(LinumWidget):
     def __init__(self, viewer: "napari.viewer.Viewer"):
         super().__init__(viewer)
         self._zarr_path = FileEdit(label="Zarr Path", mode='d')
         self._resolution = FloatSpinBox(label="Resolution (um)", value=10.0)
         self._zarr = None
-
-        # drop down with zarr or ome-zarr options
-        self._format = ComboBox(
-            label="Format",
-            choices=["zarr", "ome-zarr"],
-            value="zarr",
-        )
 
         self._type = ComboBox(
             label="Type",
@@ -41,7 +38,6 @@ class ZarrLoader(LinumWidget):
         self.extend(
             [
                 self._zarr_path,
-                self._format,
                 self._type,
                 self._resolution,
                 self._load_data_button,
@@ -53,31 +49,55 @@ class ZarrLoader(LinumWidget):
         return (res, res, res)
 
     def _load(self):
-        if self._type.value == "data":
-            self._load_zarr_data()
-        elif self._type.value == "labels":
-            self._load_zarr_labels()
-
-    def _load_zarr_data(self):
+        self._clear_message()
+        if not path.exists(self._zarr_path.value):
+            self._update_message('File does not exist')
+            return
         try:
-            zarr_path = str(self._zarr_path.value)
-            name = get_name(zarr_path)
-            self._zarr = zarr.open(zarr_path, mode="r")
-            self._viewer.add_image(self._zarr, scale=self._scale(), colormap="magma", name=name)
-            self._viewer.scale_bar.visible = True
-            self._viewer.scale_bar.unit = "um"
-            self._clear_message()
+            ext = get_extension(self._zarr_path.value)
+            if self._type.value == "labels":
+                self._load_zarr_labels()
+            elif self._type.value == "data":
+                if ext == "zarr": self._open_zarr_data()
+                elif ext == "omezarr": self._open_omezarr_data()
+                else:
+                    self._update_message('Unsupported file format')
+                    return
         except Exception as e:
             print(e)
-            self._update_message('Error loading Zarr file')
+            self._update_message('Error loading Zarr Data')
+            return
+
+        self._viewer.scale_bar.visible = True
+        self._viewer.scale_bar.unit = "um"
+
+    def _open_omezarr_data(self):
+        root = zarr.open(self._zarr_path.value, mode="r")
+        n_scales = len(root)
+        imin = root[n_scales-1][:].min()
+        imax = np.percentile(root[n_scales-1][:], 99.9)
+
+        self._viewer.open(
+            self._zarr_path.value, 
+            plugin='napari-ome-zarr', 
+            name=get_name(self._zarr_path.value), 
+            scale=self._scale(),
+            colormap="magma",
+            contrast_limits=[imin, imax]
+        )
+
+    def _open_zarr_data(self):
+        self._viewer.add_image(
+            zarr.open(self._zarr_path.value, mode="r"), 
+            scale=self._scale(), 
+            colormap="magma", 
+            name=get_name(self._zarr_path.value)
+        )
     
     def _load_zarr_labels(self):
-        try:
-            name = get_name(self._zarr_path.value)
-            self._zarr = zarr.load(self._zarr_path.value)
-            self._viewer.add_labels(self._zarr, name=name, scale=self._scale())
-            self._clear_message()
-        except Exception as e:
-            print(e)
-            self._update_message('Error loading Zarr file')
+        self._viewer.add_labels(
+            zarr.load(self._zarr_path.value), 
+            name=get_name(self._zarr_path.value), 
+            scale=self._scale()
+        )
 
