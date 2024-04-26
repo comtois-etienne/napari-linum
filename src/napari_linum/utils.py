6a3b8a6eb563625731c1c04ec2212589b93ceda8
comtois-etienne <comtois.etienne@outlook.com>
2024-04-26 12:24:43 -0400

    replace text in parenthesis

--- src/napari_linum/utils.py
+++ src/napari_linum/utils.py
@@ -1,8 +1,15 @@
 from os import path
+import re
+
 
 def get_name(fpath):
     return path.basename(fpath).split(".")[0]
 
+
 def get_extension(fpath):
     return str(path.basename(fpath).split(".")[-1]).lower()
+
+
+def replace_text_in_parenthesis(text: str, replace: str) -> str:
+    return re.sub(r'\(.*?\)', f'({replace})', text)
 
